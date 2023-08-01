d5bd93f1ad51e81d009630ea41ad2a78fe025bfb
Shannen Delaney <shannendelaney@adelphi.edu>
2023-07-31 21:01:59 -0400

    create fire sensor

--- www/app/page.js
+++ www/app/page.js
@@ -6,7 +6,8 @@ export default function Home() {
       <h1 className="text-4xl font-bold text-center">Wall of Sensors</h1>
       <ul>
         <li><a href="/debug">Debug WebSocket</a></li>
-        <li><a href="/sensors/pir">PIR Light Sensor</a></li>
+        <li><a href="/sensors/pir">PIR Light Sensor</a></li> 
+        <li><a href="/sensors/fire">Fire Sensor</a></li>
       </ul>
     </main>
   )
