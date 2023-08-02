e9ef166698db55d3e97b0a0696fa480c8e612361
Shannen Delaney <shannendelaney@adelphi.edu>
2023-08-01 22:38:25 -0400

    create links for fire

---
+++ www/app/sensors/fire/fireinfo/analog.js
@@ -0,0 +1,18 @@
+
+
+
+function Analog() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-4xl font-bold text-center">Fire Sensor</h1>
+           
+            <h2 className="text-xl font bold text-center">KY-026 FLAME SENSOR MODULE</h2>
+
+
+            <p > The KY-026 Flame Sensor module detects infrared light emitted by fire. This module has both digital and analog outputs and a potentiometer to adjust the sensitivity. Commonly used in fire detection systems</p>
+
+        </main>
+    )
+}
+
+export default Analog;
--- www/app/sensors/fire/page.js
+++ www/app/sensors/fire/page.js
@@ -1,11 +1,31 @@
 
 
+
+
 function Fire() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1 className="text-4xl font-bold text-center">Fire Sensor</h1>
+            <div className= "h-96 relative">
+               <img src="https://images.unsplash.com/photo-1546182208-1e70985e2bf3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmlyZXxlbnwwfHwwfHx8Mg%3D%3D&auto=format&fit=crop&w=600&q=60" className="w-full h-full"></img>
+            </div>
             <h2 className="text-xl font bold text-center">KY-026 FLAME SENSOR MODULE</h2>
-
+            <div className="d-flex">
+              <button
+                type="button"
+                className="inline-block rounded-full bg-green-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white">
+                    <li><a href="../fireinfo/digital">Digital Output</a></li>
+                </button>
+                  <button type="button" className="inline-block rounded-full bg-cyan-600 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white">
+                    <li><a href="../fireinfo/analog">Analog Output </a></li>
+              </button>
+              <button
+                type="button"
+                className="inline-block rounded-full bg-red-600 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white">
+                Adjust Sensor
+            </button>
+            </div>
+            
             <p > The KY-026 Flame Sensor module detects infrared light emitted by fire. This module has both digital and analog outputs and a potentiometer to adjust the sensitivity. Commonly used in fire detection systems</p>
 
         </main>
