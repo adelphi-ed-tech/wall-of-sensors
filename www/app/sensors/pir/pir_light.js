e3eb9ed3d8dcd653e6a2da779312457e8f2c9001
magpantaynic <magpantaynicks16@gmail.com>
2023-11-01 19:56:24 -0400

    testing push

--- www/app/sensors/pir/pir_light.js
+++ www/app/sensors/pir/pir_light.js
@@ -1,10 +1,12 @@
+import React from 'react';
+
 
 function Pir() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1 className="text-4xl font-bold text-center">PIR Light Sensor</h1>
         </main>
-    )
-}
+    )    
+    }
 
 export default Pir;
